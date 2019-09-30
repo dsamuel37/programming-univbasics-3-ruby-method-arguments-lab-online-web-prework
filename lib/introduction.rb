3571c0ff57824fe7fa82a7471da0297d86b5cce4
Destiny Samuel <destinysamuel37@gmail.com>
2019-09-30 19:51:24 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -9,8 +9,9 @@ introduction("Destiny")
 def introduction_with_language(name,language)
 puts "Hi, my name is #{name} and I am learning to program in #{language}"
 end 
-introduction_with_language("Destiny","Ruby")
-
+introduction_with_language("Destiny","Elixr")
+def introduction_with_language_optional(name,language = "Ruby")
+  puts "Hi my name is #{name} and I am learning to program in #{language}"
 
 
 
